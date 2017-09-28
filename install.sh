794d54914edc74cd625569f92bd38701122ff8b8
quancarry <shisuipro@gmail.com>
2017-09-28 21:16:49 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,95 @@
+# !bin/bash
+
+#Update 
+	yum -y update && yum -y upgrade 
+	
+#Change hostname
+
+	hostnamectl set-hostname VWM
+
+#Install ssh
+
+	yum -y install openssh-server openssh-clients
+	systemctl restart sshd.service
+
+#install Apache
+
+	yum -y install httpd
+	
+#install mysql(mariadb)
+
+	yum -y install mariadb-server mariadb
+	
+#config mysql
+
+	dbpass=abc@123
+	echo -e "\ny\ny\n$dbpass\n$dbpass\ny\ny\ny\ny" | /usr/bin/mysql_secure_installation
+	
+#install php
+
+	yum -y install php php-mysql
+
+#VWM path : 
+
+	#Create owner dictionary
+	
+		mkdir /var/www/html/vwm
+	
+	#Define root path
+	
+		$VM_path=/var/www/html/vwm
+
+#Enable webserver
+
+	systemctl start mariadb
+	systemctl enable mariadb.service
+	
+	systemctl enable httpd.service
+	systemctl start httpd.service
+
+#config ssl
+#
+#
+#
+#
+#
+#
+
+#Enable .htaccess
+
+	sed '/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride all/' /etc/httpd/conf/httpd.conf
+
+#Enable Authentication 
+
+	USER =VWM-user #Your username here
+	PASSWD =123 # Your password here
+	sed '/<Directory \/var\/www\html/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride AuthConfig/' /etc/httpd/conf/httpd.conf 
+	echo '$PASSWD\n$PASSWD' | htpasswd  /etc/httpd/.htpasswd $USER
+	echo AuthType Basic\nAuthName "Restricted Content"\nAuthUserFile /etc/httpd/.htpasswd\nRequire $USER > /var/www/html/vwm/.htaccess
+	apachectl restart
+	
+#Enable iptables
+
+	service iptables start
+
+#Iptable allows ports:
+	
+	iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
+	iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
+	iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
+	iptables -A INPUT -p tcp -m tcp --dport 514 -j ACCEPT
+	service iptables start
+	
+#Folder save log file
+
+	mkdir /var/log/httpd
+
+#Access log name
+	
+	sed -i '/ErrorLog "logs/error.log"/  c\ErrorLog "logs/error.log"' /etc/httpd/http.conf
+	sed -i '/CustomLog "logs/access.log" combined c\CustomLog "logs/web_access.log" combined' /etc/httpd/http.conf
+	
+# Config Logrotate
+
+	sed -i 's/$/\n "/var/log/httpd/web_access.log" /var/log/httpd/error.log{ \n rotate 5 \n size 20M}  /' /etc/logrotate.conf
+	
