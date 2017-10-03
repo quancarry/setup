c0e624febc8fa357298e36aa019bb0e36c0c0578
quancarry <shisuipro@gmail.com>
2017-10-03 09:44:03 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -91,8 +91,8 @@ func_install_centos6(){
 	echo '===== Set hostname ======'	
 	sed -n "s/HOSTNAME=.*/HOSTNAME=$server_hostname"/p /etc/sysconfig/network
 	echo $"127.0.0.1	$server_hostname $server_hostname
-	127.0.0.1	localhost.localdomain localhost
-	::1	localhost6.localdomain6 localhost6" > /etc/hosts
+	127.0.0.1	localhost localhost.localdomain localhost4 localhost4.localdomain4
+	::1	localhost localhost.localdomain localhost6 localhost6.localdomain6" > /etc/hosts
 	cat /etc/hosts | grep "$server_hostname"
 	hostname $server_hostname
 		
